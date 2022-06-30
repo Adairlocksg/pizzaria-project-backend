7681f728d4a3e2a649ccb4bc8c69cd06d0f09498
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2022-06-29 23:19:55 -0300

    Inicio do projeto de pizzaria

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from "express";
+
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
+const router = Router();
+
+//--ROTAS USER--
+router.post("/users", new CreateUserController().handle);
+
+export { router };
---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,41 @@
+import prismaClient from "../../prisma";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+
+    return { user };
+  }
+}
+export { CreateUserService };
