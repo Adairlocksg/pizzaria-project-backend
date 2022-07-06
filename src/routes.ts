07eb2a821a18570f8bf9d6631c983dbdfd777fe4
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2022-07-05 21:24:41 -0300

    Auth user and hash password

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
 //--ROTAS USER--
 router.post("/users", new CreateUserController().handle);
 
+router.post("/session", new AuthUserController().handle);
+
 export { router };
+ 
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
 
 interface UserRequest {
   name: string;
@@ -21,12 +22,13 @@ class CreateUserService {
     if (userAlreadyExists) {
       throw new Error("User already exists");
     }
-
+    const passwordHash = await hash(password, 8);
+    
     const user = await prismaClient.user.create({
       data: {
         name: name,
         email: email,
-        password: password,
+        password: passwordHash,
       },
       select: {
         id: true,
